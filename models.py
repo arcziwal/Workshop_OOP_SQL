8beecc91da2ec6eb11d995f51e57cc137d03eda1
Artur Waligora <waligoraartur95@gmail.com>
2021-12-04 20:56:36 +0100

    complete User class added

--- models.py
+++ models.py
@@ -1,8 +1,11 @@
+from clcrypto import hash_password
+
+
 class User:
-    def __init__(self, username, hashed_password):
+    def __init__(self, username="", password="", salt=""):
         self._id = -1
         self.username = username
-        self._hashed_password = hashed_password
+        self._hashed_password = hash_password(password, salt)
 
     @property
     def id(self):
@@ -12,3 +15,70 @@ class User:
     def hashed_password(self):
         return self._hashed_password
 
+    def set_password(self, password, salt=""):
+        self._hashed_password = hash_password(password, salt)
+
+    @hashed_password.setter
+    def hashed_password(self, password):
+        self.set_password(password)
+
+    def save_to_db(self, cursor):
+        if self._id == -1:
+            sql = """INSERT INTO users(username, hashed_password)
+                        VALUES(%s, %s) RETURNING id"""
+            values = (self.username, self.hashed_password)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()[0]
+            return True
+        else:
+            sql = """UPDATE users SET username=%s, hashed_password=%s
+                        WHERE id=%s"""
+            values = (self.username, self.hashed_password, self._id)
+            cursor.execute(sql, values)
+            return True
+
+    def load_user_by_id(self, cursor, id_):
+        sql = "SELECT id, username, hashed_password FROM users WHERE id=%s"
+        cursor.execute(sql, (id_,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            return None
+
+    def load_user_by_username(self, cursor, username):
+        sql = "SELECT id, username, hashed_password FROM users WHERE username=%s"
+        cursor.execute(sql, (username,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
+        else:
+            return None
+
+    @staticmethod
+    def load_all_users(cursor):
+        sql = "SELECT id, username, hashed_password FROM users"
+        users = []
+        cursor.execute(sql)
+        for row in cursor.fetchall():
+            id_, username, hashed_password = row
+            loaded_user = User()
+            loaded_user._id = id_
+            loaded_user.username = username
+            loaded_user._hashed_password = hashed_password
+            users.append(loaded_user)
+        return users
+
+    def delete(self, cursor):
+        sql = "DELETE FROM users WHERE id=%s"
+        cursor.execute(sql)
+        self._id = -1
+        return True
