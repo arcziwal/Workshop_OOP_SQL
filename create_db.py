9b8a20df3a081c636a983b4032d5cc311909833a
Artur Waligora <waligoraartur95@gmail.com>
2021-12-04 20:13:19 +0100

    script creating db and tables done

--- create_db.py
+++ create_db.py
@@ -3,12 +3,22 @@ from psycopg2.errorcodes import DUPLICATE_DATABASE, DUPLICATE_TABLE
 
 user='postgres'
 password = 'coderslab'
-create_db_sql = """CREATE DATABASE Workshop;"""
-create_table_sql = """CREATE TABLE users
+create_db_sql = """CREATE DATABASE workshop;"""
+create_table_users_sql = """CREATE TABLE users
 (
     id serial,
     username varchar(255) unique,
     hashed_password varchar(80),
+    PRIMARY KEY(id)
+);
+"""
+create_table_messages_sql = """CREATE TABLE messages
+(
+    id serial,
+    FOREIGN KEY(id)
+    REFERENCES users(id),
+    creation_date timestamp,
+    text varchar(255),
     PRIMARY KEY(id)
 );
 """
@@ -17,18 +27,21 @@ try:
     cnx = connect(user=user, password=password, host='localhost')
     print("Connection established")
     cursor = cnx.cursor()
+    cnx.autocommit = True
     cursor.execute(create_db_sql)
-    cnx.commit()
-    cursor.execute(create_table_sql)
-    cnx.commit()
+    cursor.close()
+    cnx.close()
+    cnx = connect(user=user, password=password, host='localhost', database='workshop')
+    cursor = cnx.cursor()
+    cnx.autocommit = True
+    cursor.execute(create_table_users_sql)
+    cursor.execute(create_table_messages_sql)
 except OperationalError as e:
     if e.pgcode == DUPLICATE_DATABASE:
         print("Requested database already exists")
     elif e.pgcode == DUPLICATE_TABLE:
         print("Requested table already esist")
-    print("Connection cannot be established")
-
-
-
-
-
+    print(f"Connection cannot be established. {e}")
+else:
+    cursor.close()
+    cnx.close()
